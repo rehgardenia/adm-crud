b6d04a8547a81b47642454df77dfa2f4e8aa1287
Renata Gardenia <r.gardenia06@gmail.com>
2024-07-08 10:25:41 -0300

    conteúdo principal

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Aqui eu coloquei em JS porque não sei qual linguagem vamos usar 
 // Só uma demonstração de como ficaria o layout do menu
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+    const aside = document.querySelector('aside');
     const toggleMenu = document.getElementById('toggle-menu');
-    const barraLateral = document.querySelector('aside');
 
-    toggleMenu.addEventListener('click', function () {
-        barraLateral.classList.toggle('expande');
+    toggleMenu.addEventListener('click', () => {
+        aside.classList.toggle('expande');
     });
 });
